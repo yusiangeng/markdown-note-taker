f915beb0e2084d44615c66acc4a1df3dff2557ae
yusiangeng <engyusiang@icloud.com>
2022-09-20 13:53:18 +0800

    Save files

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -22,6 +23,7 @@ func main() {
 	win := a.NewWindow("Markdown Note Taker")
 
 	edit, preview := cfg.makeUi()
+	cfg.createMenuItems(win)
 
 	win.SetContent(container.NewHSplit(edit, preview))
 
@@ -43,4 +45,47 @@ func (app *Config) makeUi() (*widget.Entry, *widget.RichText) {
 	edit.OnChanged = preview.ParseMarkdown
 
 	return edit, preview
+}
+
+func (app *Config) createMenuItems(win fyne.Window) {
+	openMenuItem := fyne.NewMenuItem("Open...", func() {})
+
+	saveMenuItem := fyne.NewMenuItem("Save", func() {})
+	app.SaveMenuItem = saveMenuItem
+	app.SaveMenuItem.Disabled = true
+
+	saveAsMenuItem := fyne.NewMenuItem("Save as...", app.saveAsFunc(win))
+
+	fileMenu := fyne.NewMenu("File", openMenuItem, saveMenuItem, saveAsMenuItem)
+
+	menu := fyne.NewMainMenu(fileMenu)
+
+	win.SetMainMenu(menu)
+}
+
+func (app *Config) saveAsFunc(win fyne.Window) func() {
+	return func() {
+		saveDialog := dialog.NewFileSave(func(write fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+
+			// handle cancel button
+			if write == nil {
+				return
+			}
+
+			// save file
+			write.Write([]byte(app.EditWidget.Text))
+			app.CurrentFile = write.URI()
+
+			defer write.Close()
+
+			win.SetTitle(win.Title() + " - " + write.URI().Name())
+			app.SaveMenuItem.Disabled = false
+		}, win)
+
+		saveDialog.Show()
+	}
 }
